ee5fb4cfa0bd9ec3a0e4052ef249c223c856beec
BSL16 <bambasakho02@gmail.com>
2022-12-19 15:46:57 +0100

    Ajout de l'extension de la classe Consocarbone qui est Train_Bus et l'ajout de ce que Marc m'a envoyé dernièrement avec les modifications

---
+++ Projet_SAKHO_KASPAR/ConsoCarbone/Train_Bus.java
@@ -0,0 +1,33 @@
+package consoCarbone;
+/** Train_Bus est une extension de la classe Consocarbone. Cette classe herite de Transport qui elle meme herite de Consocarbone*/
+public class Train_Bus extends Transport {
+    /** Constructeur par défaut */
+    public Train_Bus(){
+        super();
+    }
+    /**
+     * Constructeur de Train Bus
+     * @param possede
+     * @param taille
+     * @param kilomAnnee
+     * @param amortissement
+     */
+    public Train_Bus(boolean possede, Taille taille, int kilomAnnee, int amortissement){
+        super(possede, taille, kilomAnnee, amortissement);
+    }
+    /** modifie la valeur de l impact en ajoutant 85 à l empreinte du transport si l utilisateur a une voiture, sinon impact est à 85*/
+    @Override   
+    public void EmpreinteC(){
+        if(get_possede()){
+            impact=(get_kilomAnne())*(1.93*0.0001) + get_taille().get_emission()/get_amortissement() + 85;
+        }
+        else {
+            impact=85;
+            }
+        }
+    /** @return String renvoie les caractéristiques de l'utilisateur vis a vis de son utilisation de Train */
+    @Override
+    public String toString(){ 
+    return "Train_Bus [ ID: "+id+",  Impact en TCO2: "+impact+" ]";
+    }
+}
